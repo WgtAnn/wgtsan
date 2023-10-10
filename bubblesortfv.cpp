aa4475005cf3086ad18d83e0f6b313bb4afd6035
WgtAnn <144911036+WgtAnn@users.noreply.github.com>
2023-10-10 19:55:22 +0800

    Add files via upload

---
+++ bubblesortfv.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[50000], n,tmp;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	if (a[i]<50000 && a[i]>-50000) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j < n; j++) {
+				if (a[j] > a[j + 1]) {
+					tmp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = tmp;
+				}
+			}
+		}
+		for (int i = 1; i <= n; i++)
+			cout << a[i] << " ";
+	}
+}
